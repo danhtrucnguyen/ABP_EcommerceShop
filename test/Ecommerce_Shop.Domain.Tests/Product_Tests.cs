1694c09affc671930698a3346b1638f55d4cf8b9
DanhTruc2002 <danhtrucsaison@gmail.com>
2025-09-10 14:34:10 +0700

    add domain product test

---
+++ test/Ecommerce_Shop.Domain.Tests/Product_Tests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce_Shop
+{
+    using Ecommerce_Shop.Entities;
+    using Shouldly;
+    using System;
+    using Xunit;
+
+    
+
+    public class Product_Tests
+    {
+        [Fact]
+        public void Ctor_Should_Set_Properties()
+        {
+            var id = Guid.NewGuid();
+            var p = new Product(id, "Name", 123.45m, Guid.Empty);
+
+            p.Id.ShouldBe(id);
+            p.Name.ShouldBe("Name");
+            p.Price.ShouldBe(123.45m);
+            p.Description.ShouldBeNull();
+            p.CategoryId.ShouldBe(Guid.Empty);
+        }
+
+        [Fact]
+        public void Update_Should_Change_Properties()
+        {
+            var p = new Product(Guid.NewGuid(), "N1", 10m, null);
+            var newCat = Guid.NewGuid();
+            p.Update("N2", 20m, "d2", newCat);
+
+            p.Name.ShouldBe("N2");
+            p.Price.ShouldBe(20m);
+            p.Description.ShouldBe("d2");
+            p.CategoryId.ShouldBe(newCat);
+        }
+    }
+
+}
